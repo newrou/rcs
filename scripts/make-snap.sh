4fbc5509bce8cf9aa9dbb333e893cb503b64297d
root <root@debian-BULLSEYE-live-builder-AMD64>
2022-10-29 18:11:55 +0700

    rename def pool to Snuff

--- scripts/make-snap.sh
+++ scripts/make-snap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+fs=$1
+nsnap=$2
+pool='Snuff'
+
 d=`date +%d.%m.%Y-%H.%M.%S`
 echo "make snap-$d"
-/usr/sbin/zfs snapshot Oasis/fs1@snap-$d
+/usr/sbin/zfs snapshot $pool/$fs@snap-$d
 
-lst=`/usr/sbin/zfs list -t snapshot Oasis/fs1 | head -n -7 | tail -n +2 | awk '{print $1}'`
+lst=`/usr/sbin/zfs list -t snapshot $pool/$fs | head -n -$nsnap | tail -n +2 | awk '{print $1}'`
 for i in $lst
 do
    echo "remove $i"
